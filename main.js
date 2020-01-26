e5ea5e93708b87d170b18a208789e5f62b795a75
alberto ortiz <albertricking@gmail.com>
2020-01-26 01:10:59 -0600

    version1

---
+++ main.js
@@ -0,0 +1,5 @@
+function abrirWhatsapp(){
+
+    window.open('https://api.whatsapp.com/send?phone=5216623162968&text=Hola Me Interesa el Paquete de Lavado 1', '', 'width=500,height=500,scrollbars=NO,statusbar=NO,left=500,top=200');
+
+    }
